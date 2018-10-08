eac6f1730f27afc6a8a71f7780fc91547e8d4db7
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2018-10-08 14:46:54 +0500

    Issue #3002872 Support to add styles and scripts in head tag

--- src/Form/HeaderForm.php
+++ src/Form/HeaderForm.php
@@ -7,49 +7,49 @@ use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Provide settings page for adding CSS/JS after the start of body tag.
+ * Provide settings page for adding CSS/JS before the end of body tag.
  */
-class BodyForm extends ConfigFormBase {
+class HeaderForm extends ConfigFormBase {
 
   /**
    * Implements FormBuilder::getFormId.
    */
   public function getFormId() {
-    return 'hfs_body_settings';
+    return 'hfs_header_settings';
   }
 
   /**
    * Implements ConfigFormBase::getEditableConfigNames.
    */
   protected function getEditableConfigNames() {
-    return ['hfs_body_scripts.settings'];
+    return ['hfs_header_scripts.settings'];
   }
 
   /**
    * Implements FormBuilder::buildForm.
    */
   public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
-    $body_section = $this->config('hfs_body_scripts.settings')->get();
+    $header_section = $this->config('hfs_header_scripts.settings')->get();
 
-    $form['hfs_body'] = [
+    $form['hfs_header'] = [
       '#type'        => 'fieldset',
-      '#title'       => $this->t('Add Scripts and Styles in body'),
-      '#description' => $this->t('All the defined scripts and styles in this section would be added next to <strong>body</strong> tag.'),
+      '#title'       => $this->t('Add Scripts and Styles in Header'),
+      '#description' => $this->t('All the defined scripts and styles in this section would be added just before closing the <strong>body</strong> tag.'),
     ];
 
-    $form['hfs_body']['styles'] = [
+    $form['hfs_header']['styles'] = [
       '#type'          => 'textarea',
-      '#title'         => $this->t('Body Styles'),
-      '#default_value' => isset($body_section['styles']) ? $body_section['styles'] : '',
+      '#title'         => $this->t('Header Styles'),
+      '#default_value' => isset($header_section['styles']) ? $header_section['styles'] : '',
       '#description'   => $this->t('<p>You can add multiple <strong>stylesheets</strong> here with multiple ways, For example: </p><p>1. &lt;link type="text/css" rel="stylesheet" href="http://www.example.com/style.css" media="all" /&gt;</p><p> 2. &lt;link type="text/css" rel="stylesheet" href="/style.css" media="all" /&gt;</p><p> 3. &lt;style&gt;#header { color: grey; }&lt;/style&gt;</p>'),
       '#rows'          => 10,
     ];
 
-    $form['hfs_body']['scripts'] = [
+    $form['hfs_header']['scripts'] = [
       '#type'          => 'textarea',
-      '#title'         => $this->t('Body Scripts'),
-      '#default_value' => isset($body_section['scripts']) ? $body_section['scripts'] : '',
-      '#description'   => $this->t('<p>On mostly sites, this section is used to add the <strong>Google Tag Manager</strong>. <strong>Like:</strong></p><p>1. &lt;!-- Google Tag Manager --&gt;&lt;noscript&gt;<strong>Write Your code here</strong>&lt;/script&gt;&lt;!-- End Google Tag Manager --&gt;</p><p>You can also add multiple <strong>scripts</strong> here with multiple ways, For example: </p><p>1. &lt;script type="text/javascript" src="http://www.example.com/script.js"&gt;&lt;/script&gt;</p><p> 2. &lt;script type="text/javascript" src="/script.js"&gt;&lt;/script&gt;</p><p> 3. &lt;script type="text/javascript"&gt;console.log("HFS Body");&lt;/script&gt;</p>'),
+      '#title'         => $this->t('Header Scripts'),
+      '#default_value' => isset($header_section['scripts']) ? $header_section['scripts'] : '',
+      '#description'   => $this->t('<p>You can add multiple <strong>scripts</strong> here with multiple ways, For example: </p><p>1. &lt;script type="text/javascript" src="http://www.example.com/script.js"&gt;&lt;/script&gt;</p><p> 2. &lt;script type="text/javascript" src="/script.js"&gt;&lt;/script&gt;</p><p> 3. &lt;script type="text/javascript"&gt;console.log("HFS Header");&lt;/script&gt;</p>'),
       '#rows'          => 10,
     ];
 
@@ -65,7 +65,7 @@ class BodyForm extends ConfigFormBase {
     $values = $form_state->getValues();
 
     $this->configFactory()
-      ->getEditable('hfs_body_scripts.settings')
+      ->getEditable('hfs_header_scripts.settings')
       ->set('styles', $values['styles'])
       ->set('scripts', $values['scripts'])
       ->save();
